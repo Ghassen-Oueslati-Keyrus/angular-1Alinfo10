359b8cfeded5d9aa32fc48e72b9d31e1d6f2cd8c
Oueslati-Ghassen <ghassen.oueslati@keyrus.com>
2023-05-17 11:01:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { ProductsComponentComponent } from './products-component/products-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListProductComponent,
+    ProductsComponentComponent
   ],
   imports: [
     BrowserModule,
